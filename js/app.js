5ff84d1a81dee7e23bdadf6edc50f771839e8e90
earl <earl@digitalocean.com>
2016-07-29 09:37:11 -0400

    fixing animations and mobile styles

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
 $(document).foundation();
+$(function(){
+
+function down(){
+  $('#wave').addClass('hidden');
+  $('#down').removeClass('hidden');
+}
+function nice(){
+  $('#down').addClass('hidden');
+  $('#nice').removeClass('hidden');
+}
+var scrolling;
+    scrolling = setTimeout(function(){ down() }, 2000);
+
+var scrollingSecond;
+    scrollingSecond = setTimeout(function(){ nice() }, 4000);
+
+$(document).on('scroll', function() {
+    if($(this).scrollTop()>=130){
+       clearTimeout(scrolling);
+       clearTimeout(scrollingSecond);
+    }
+})
+
+});
--- js/switch.js
+++ js/switch.js
@@ -1,8 +1,19 @@
 $(function(){
-  $('.em-wave').click(function(){
-    $(this).addClass('small').delay(1000).queue(function(){
-      $(this).removeClass("em-wave").addClass("em-mag").removeClass('small').dequeue();
-    });
-  });
 
+function down(){
+  $('#wave').addClass('hidden');
+  $('#down').removeClass('hidden');
+}
+function nice(){
+  $('#down').addClass('hidden');
+  $('#nice').removeClass('hidden');
+}
+var myVar;
+    myVar = setTimeout(function(){ console.log("Hello") }, 3000);
+
+console.log('yes')
+
+$( window ).scroll(function() {
+  clearTimeout(scrolling);
+}
 });
