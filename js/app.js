b456412be73d7fafcd495d5f79445966f1d1ea77
earl <earl@digitalocean.com>
2016-07-30 15:44:59 -0400

    adding animations

--- js/app.js
+++ js/app.js
@@ -2,13 +2,28 @@ $(document).foundation();
 $(function(){
 
 function down(){
-  $('#wave').addClass('hidden');
-  $('#down').removeClass('pre-hidden');
+  $('#wave').velocity(
+  {opacity:0, left:'-30px'},
+  {duration: 500}
+  );
+  $('#down').velocity(
+  {opacity:1, left:'0px'},
+  {duration: 500}
+  );
 }
 function nice(){
-  $('#wave').addClass('hidden');
-  $('#down').addClass('hidden');
-  $('#nice').removeClass('pre-hidden');
+  $('#wave').velocity(
+  {opacity:0, left:'-30px'},
+  {duration: 500}
+  );
+  $('#down').velocity(
+  {opacity:0, left:'-30px'},
+  {duration: 500}
+  );
+  $('#nice').velocity(
+  {opacity:1, left:'0px'},
+  {duration: 500}
+  );
 }
 var scrolling;
     scrolling = setTimeout(function(){ down() }, 6000);
@@ -20,18 +35,28 @@ $(document).on('scroll', function() {
     }
 })
 
-$('body').css('display', 'none');
 
-$('body').fadeIn(1000);
+$('.border').velocity("fadeIn",
+  { 
+    duration: 1000
+  });
 
+$('#page,#next,.opening,.goodbye,#work > .row:nth-child(odd),#work > .row:nth-child(even)').velocity({ opacity: 1,left:0},{duration:900,delay:800});
 
-$('a').click(function() {
+$('a').click(function(event) {
 
   event.preventDefault();
 
   newLocation = this.href;
 
-  $('body').fadeOut(1000, newpage);
+ $('.border').velocity("fadeOut",
+  { 
+    duration: 900,
+    complete:  function() { newpage() }
+  });
+
+$('#page,.opening,.goodbye,#work,#next').velocity({ opacity: 0,left:'-30px'},{duration:800});
+
 
 });
 
