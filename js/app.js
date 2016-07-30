d106c24d99800eb92b1f519eeea222ce57b64c7c
earl <earl@digitalocean.com>
2016-07-30 10:51:14 -0400

    case studies

--- js/app.js
+++ js/app.js
@@ -20,4 +20,30 @@ $(document).on('scroll', function() {
     }
 })
 
+$('body').css('display', 'none');
+
+$('body').fadeIn(1000);
+
+
+$('a').click(function() {
+
+  event.preventDefault();
+
+  newLocation = this.href;
+
+  $('body').fadeOut(1000, newpage);
+
 });
+
+function newpage() {
+
+  window.location = newLocation;
+
+}
+
+});
+
+
+
+
+
