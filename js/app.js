407ff7c3aa2909c00b0d07e92eb206fdb3b4b3af
earl <earl@digitalocean.com>
2016-07-30 16:29:36 -0400

    small changes

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(document).foundation();
-$(function(){
+function init(){
 
 function down(){
   $('#wave').velocity(
@@ -65,7 +65,78 @@ function newpage() {
   window.location = newLocation;
 
 }
+$('.player').get(0).play();
 
+};
+function waitForWebfonts(fonts, callback) {
+    var loadedFonts = 0;
+    for(var i = 0, l = fonts.length; i < l; ++i) {
+        (function(font) {
+            var node = document.createElement('span');
+            // Characters that vary significantly among different fonts
+            node.innerHTML = 'giItT1WQy@!-/#';
+            // Visible - so we can measure it - but not on the screen
+            node.style.position      = 'absolute';
+            node.style.left          = '-10000px';
+            node.style.top           = '-10000px';
+            // Large font size makes even subtle changes obvious
+            node.style.fontSize      = '300px';
+            // Reset any font properties
+            node.style.fontFamily    = 'sans-serif';
+            node.style.fontVariant   = 'normal';
+            node.style.fontStyle     = 'normal';
+            node.style.fontWeight    = 'normal';
+            node.style.letterSpacing = '0';
+            document.body.appendChild(node);
+
+            // Remember width with no applied web font
+            var width = node.offsetWidth;
+
+            node.style.fontFamily = font;
+
+            var interval;
+            function checkFont() {
+                // Compare current width with original width
+                if(node && node.offsetWidth != width) {
+                    ++loadedFonts;
+                    node.parentNode.removeChild(node);
+                    node = null;
+                }
+
+                // If all fonts have been loaded
+                if(loadedFonts >= fonts.length) {
+                    if(interval) {
+                        clearInterval(interval);
+                    }
+                    if(loadedFonts == fonts.length) {
+                        callback();
+                        return true;
+                    }
+                }
+            };
+
+            if(!checkFont()) {
+                interval = setInterval(checkFont, 50);
+            }
+        })(fonts[i]);
+    }
+};
+$(document).ready(function() {
+  var check1 = false, check2 = false;
+  waitForWebfonts(['Cinetype-Regular'], function() {
+    if (check2) {
+
+      init();
+    }
+    check1 = true;
+  })
+  waitForWebfonts(['Walsheim'], function() {
+    if (check1) {
+
+      init();
+    }
+    check2 = true;
+  })
 });
 
 
